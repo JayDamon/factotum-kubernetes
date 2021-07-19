1a98e6878b95a88d86cce065993b6ef52e94dccd
= <Jay.n.damon@gmail.com>
2021-07-19 18:12:20 -0400

    Update way we handle arguments

--- moneymaker/launch-app.sh
+++ moneymaker/launch-app.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-namespace=$1;
+env=$1;
 
-if [ -n "$namespace" ]
-    then
-        kubectl apply -f namespaces.yml
-
-        kubectl apply -f config-map.yml
-
-        kubectl apply -f moneymaker-certs.yml
-
-        kubectl apply -f moneymaker-app.yml -n=$namespace
-
-        kubectl apply -f ingress-config.yml
-    else
-        echo "Namespace argument not provided";
+if [[ -z "$env" ]]
+then
+    echo "env argument not provided";
+    exit 128
+elif [[ "dev" = $env ]] || [[ "prod" = $env ]]
+then
+    namespace="mm-$env"
+else
+    echo "Invalid argument provided: $env"
+    exit 128
 fi
+
+echo "Launching app in env $namespace"
+
+kubectl apply -f envs.yml
+
+kubectl apply -f config-map.yml
+
+kubectl apply -f moneymaker-certs.yml
+
+kubectl apply -f moneymaker-app.yml -n=$namespace
+
+kubectl apply -f ingress-config.yml
